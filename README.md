16c95368d67065719dce350ced482948a8b1428a
NachoToast <32235595+NachoToast@users.noreply.github.com>
2025-07-18 17:01:43 +1200

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Less Sad Night Owls ![image](https://img.shields.io/endpoint.svg?url=https%3A%2F%2Fshieldsio-steam-workshop.jross.me%2F3498631113)
+
+A RimWorld mod that modifies when pawns with the "Night Owl" trait complain about being awake during the day.
+
+Find it [here](https://steamcommunity.com/sharedfiles/filedetails/?id=3498631113) on the Steam workshop.
---
+++ Source/LessSadNightOwls2/1.6/LessSadNightOwls2.cs
@@ -0,0 +1,49 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace LessSadNightOwls2;
+
+[StaticConstructorOnStartup]
+public class LessSadNightOwls2
+{
+    static LessSadNightOwls2()
+    {
+        Harmony harmony = new("NachoToast.LessSadNightOwls2");
+
+        harmony.Patch(
+            original: AccessTools.Method(
+                type: typeof(ThoughtWorker_IsDayForNightOwl),
+                name: "CurrentStateInternal"),
+            postfix: new HarmonyMethod(
+                methodType: typeof(LessSadNightOwls2),
+                methodName: nameof(CurrentStateInteral_Postfix)));
+    }
+
+    private static void CurrentStateInteral_Postfix(Pawn p, ref ThoughtState __result)
+    {
+        if (!__result.Active)
+        {
+            // already inactive
+            return;
+        }
+
+        if (p.IsPrisoner)
+        {
+            // prisoner - suppress
+            __result = false;
+            return;
+        }
+
+        for (int i = 11; i <= 18; i++)
+        {
+            if (p.timetable.GetAssignment(i) != TimeAssignmentDefOf.Sleep)
+            {
+                // not set as sleep, don't suppress
+                return;
+            }
+        }
+
+        __result = false;
+    }
+}
